7db0da65f656ff346876ca600a3ca2bcf65d6283
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-11-30 22:48:34 +0100

    Update index.html and index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,37 @@
 import 'reset-css';
 import '../scss/styles.scss';
+import scoreList from './modules/scoreList.js';
+import renderScores from './modules/renderScore.js';
+
+const scores = [
+  {
+    user: 'Name',
+    score: 100,
+  },
+  {
+    user: 'Name',
+    score: 20,
+  },
+  {
+    user: 'Name',
+    score: 50,
+  },
+  {
+    user: 'Name',
+    score: 78,
+  },
+  {
+    user: 'Name',
+    score: 125,
+  },
+  {
+    user: 'Name',
+    score: 77,
+  },
+  {
+    user: 'Name',
+    score: 42,
+  },
+];
+
+renderScores(scoreList(scores));
