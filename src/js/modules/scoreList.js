a8c897024c5a702fd2087f892c807e10d58de25f
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-12-03 11:01:27 +0100

    Update scoreList function

--- src/js/modules/scoreList.js
+++ src/js/modules/scoreList.js
@@ -1,6 +1,6 @@
 const scoreList = (list) => {
   if (list.length === 0) {
-    return '<p class="scoreboard__score-item">Name: 100</p>';
+    return '<p class="scoreboard__score-item empty">No score</p>';
   }
   return list.map((item) => `<p class="scoreboard__score-item">${item.user}: ${item.score}</p>`).join('');
 };
