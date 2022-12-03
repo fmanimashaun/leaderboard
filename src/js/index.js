60c76e72f05a96df30c9cd822413ad4ebc1aea3f
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-12-03 11:02:14 +0100

    Update the index.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,36 +2,38 @@ import 'reset-css';
 import '../scss/styles.scss';
 import scoreList from './modules/scoreList.js';
 import renderScores from './modules/renderScore.js';
+import fetchList from './modules/fetchList.js';
+import postScore from './modules/postScore.js';
 
-const scores = [
-  {
-    user: 'Name',
-    score: 100,
-  },
-  {
-    user: 'Name',
-    score: 20,
-  },
-  {
-    user: 'Name',
-    score: 50,
-  },
-  {
-    user: 'Name',
-    score: 78,
-  },
-  {
-    user: 'Name',
-    score: 125,
-  },
-  {
-    user: 'Name',
-    score: 77,
-  },
-  {
-    user: 'Name',
-    score: 42,
-  },
-];
+const scoreForm = document.querySelector('.add__score-form');
+const refreshBtn = document.querySelector('.scoreboard__refresh-btn');
 
-renderScores(scoreList(scores));
+let scoreListArray = localStorage.getItem('scores') ? JSON.parse(localStorage.getItem('scores')) : [];
+
+renderScores(scoreList(scoreListArray));
+
+refreshBtn.addEventListener('click', () => {
+  fetchList().then((data) => {
+    renderScores(scoreList(data.result));
+    localStorage.setItem('scores', JSON.stringify(data.result));
+  });
+});
+
+scoreForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const userName = scoreForm.querySelector('#name').value;
+  const userScore = scoreForm.querySelector('#score').value;
+  const scoreObj = {
+    user: userName,
+    score: userScore,
+  };
+  postScore(scoreObj).then(() => {
+    fetchList().then((data) => {
+      localStorage.setItem('scores', JSON.stringify(data.result));
+    }).then(() => {
+      scoreListArray = JSON.parse(localStorage.getItem('scores'));
+      renderScores(scoreList(scoreListArray));
+    });
+    scoreForm.reset();
+  });
+});
