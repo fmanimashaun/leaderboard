efb40b8369e4cbb997a6bb8ae0b97d987e2f80c6
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2023-03-11 20:35:52 +0100

    Update getScore function

--- src/js/modules/getScores.js
+++ src/js/modules/getScores.js
@@ -1,7 +1,12 @@
 const getScores = async (url) => {
+  // get scores from API
   const response = await fetch(url);
+
+  // parse response to JSON
   const data = await response.json();
-  return data;
+
+  // add scores to local storage
+  localStorage.setItem('score', JSON.stringify(data.result));
 };
 
 export default getScores;
